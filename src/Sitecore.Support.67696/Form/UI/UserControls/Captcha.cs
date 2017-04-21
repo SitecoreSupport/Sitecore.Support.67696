8460491541d255464fd7a084e3da61096a752fdb
DK\AMCH <amch@sitecore.net>
2017-04-21 07:33:44 +0300

    commit patch

---
+++ src/Sitecore.Support.67696/Form/UI/UserControls/Captcha.cs
@@ -0,0 +1,30 @@
+﻿using Sitecore.Data.Items;
+using Sitecore.Links;
+using Sitecore.Resources.Media;
+using System;
+
+namespace Sitecore.Support.Form.UI.UserControls
+{
+  public class Captcha : Sitecore.Form.UI.UserControls.Captcha
+  {
+    protected override void OnInit(EventArgs e)
+    {
+      base.OnInit(e);
+      //modified part of code: 
+      if (LinkManager.AlwaysIncludeServerUrl)
+      {
+        Item innerItem = Sitecore.Context.Database.GetItem("/sitecore/media library/web forms for marketers/icons/refresh");
+        if (innerItem != null)
+        {
+          base.captchaCode.CaptchaRefreshButton.ImageUrl = MediaManager.GetMediaUrl(new MediaItem(innerItem));
+        }
+        innerItem = Sitecore.Context.Database.GetItem("/sitecore/media library/web forms for marketers/icons/loudspeaker");
+        if (innerItem != null)
+        {
+          base.captchaCode.CaptchaPlayButton.ImageUrl = MediaManager.GetMediaUrl(new MediaItem(innerItem));
+        }
+      }
+      //end of the modified part
+    }
+  }
+}
---
+++ src/Sitecore.Support.67696/sitecore modules/Web/Web Forms for Marketers/UI/UserControl/Captcha.ascx.cs
@@ -0,0 +1,12 @@
+﻿namespace Sitecore.Support.sitecore_modules.Web.Web_Forms_for_Marketers.UI.UserControl
+{
+  using System;
+
+  public partial class Captcha : System.Web.UI.UserControl
+  {
+    private void Page_Load(object sender, EventArgs e)
+    {
+      // Put user code to initialize the page here
+    }
+  }
+}
